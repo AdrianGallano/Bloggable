3ee4cc5302f0767731e042632f8f026ea0226193
Adrian <adriangallanoengr@gmail.com>
2021-09-11 18:00:42 +0800

    Latest section done

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,13 @@ import uuid
 from django.contrib.auth.models import User
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200, blank=True, null=True)
+    title = models.CharField(max_length=40, blank=True, null=True)
     content = models.TextField(blank=True, null=True)
     min_read = models.CharField(max_length=20, null=True)
     description = models.CharField(max_length=300, blank=True, null=True)
     date_published = models.DateField(auto_now=True)
     last_updated = models.DateField(auto_now_add=True)
-    image = models.ImageField(null=True,blank=True, default='../../../static/images/default.png')
+    image = models.ImageField(null=True,blank=True, default='default.png')
     best = models.BooleanField(default=False)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Blog
-
+import os
+from django.conf import settings
 def index(request):
-    blog = Blog.objects.all()[0] 
+    blog = Blog.objects.order_by('date_published')[0] 
     template = 'blog/index.html'
-    context = {'blog':blog}
-    print('this', blog)
+    context = {'blog':blog,}
     return render(request, template, context)
 
+def blogs(request, pk):
+    return HttpResponse('Blog' + str(pk))
